da08a070edfc0bc5aef490d25f7841b513e770fb
wglee <amd0223@gmail.com>
2023-06-27 00:06:25 +0900

    섹션 6. 로그인 처리1 - 쿠키, 세션
    8.로그인 처리하기 - 직접 만든 세션 적용
    9.로그인 처리하기 - 서블릿 HTTP 세션1
    10.로그인 처리하기 - 서블릿 HTTP 세션2
    11.세션 정보와 타임아웃 설정

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -1,33 +1,32 @@
 package hello.login.web;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
+import lombok.RequiredArgsConstructor;
 
-@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
 	private final MemberRepository memberRepository;
+	private final SessionManager sessionManager;
 	
 //    @GetMapping("/")
     public String home() {
         return "home";
     }
     
-    @GetMapping("/")
+//    @GetMapping("/")
     public String loginHome(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
     	
     	if(memberId == null) {
@@ -35,24 +34,59 @@ public class HomeController {
     	}
     		
     	Member loginMember = memberRepository.findById(memberId);
+    	
     	if(loginMember == null) {
     		return "home";
     	}
     	
     	model.addAttribute("member", loginMember);
-    	
     	return "loginHome";
     }
     
-    @PostMapping("/logout")
-    public String logout(HttpServletResponse response) {
-    	expireCookie(response, "memberId");
-    	return "redirect:/";
+//    @GetMapping("/")
+    public String loginHomeV2(HttpServletRequest request, Model model) {
+    	
+    	//세션 관리자에 저장된 회원 정보 조회
+    	Member member = (Member)sessionManager.getSession(request);
+    	
+    	if(member == null) {
+    		return "home";
+    	}
+    	
+    	model.addAttribute("member", member);
+    	return "loginHome";
     }
     
-    private void expireCookie(HttpServletResponse response, String cookieName) {
-    	Cookie cookie = new Cookie("memberId", null);
-    	cookie.setMaxAge(0);
-    	response.addCookie(cookie);
+//    @GetMapping("/")
+    public String loginHomeV3(HttpServletRequest request, Model model) {
+    	
+    	//세션 관리자에 저장된 회원 정보 조회. 최초 접속 사용자의 경우 세션이 없기때문에 false.
+    	HttpSession session = request.getSession(false);
+    	
+    	if(session == null) {
+    		return "home";
+    	}
+    	
+    	Member loginMember =  (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+    	
+    	if(loginMember == null) {
+    		return "home";
+    	}
+    	
+    	model.addAttribute("member", loginMember);
+    	return "loginHome";
     }
+    
+    @GetMapping("/")
+    public String loginHomeV3Spring(
+    		@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+    	
+    	if(loginMember == null) {
+    		return "home";
+    	}
+    	
+    	model.addAttribute("member", loginMember);
+    	return "loginHome";
+    }
+
 }
--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -1,7 +1,9 @@
 package hello.login.web.login;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
+import hello.login.web.SessionConst;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -19,13 +23,14 @@ import lombok.RequiredArgsConstructor;
 public class LoginController {
 
 	private final LoginService loginService;
+	private final SessionManager sessionManager;
 	
 	@GetMapping("/login")
 	public String addForm(@ModelAttribute LoginForm form) {
 		return "/login/loginForm";
 	}
 
-	@PostMapping("/login")
+//	@PostMapping("/login")
 	public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
 		if(bindingResult.hasErrors()) {
 			return "/login/loginForm";
@@ -43,5 +48,72 @@ public class LoginController {
 		response.addCookie(cookie);
 		
 		return "redirect:/";
+	}
+	
+//	@PostMapping("/login")
+	public String loginV2(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
+		if(bindingResult.hasErrors()) {
+			return "/login/loginForm";
+		}
+		
+		Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+		
+		if(loginMember == null) {
+			bindingResult.reject("loginFailed", "아이이 또는 비밀번호가 맞지 않습니다.");
+			return "/login/loginForm";
+		}
+		
+		//세션 관리자를 통해 세션을 생성하고, 회원 데이터 보관
+		sessionManager.createSession(loginMember, response);
+		
+		return "redirect:/";
+	}
+	
+	@PostMapping("/login")
+	public String loginV3(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
+		if(bindingResult.hasErrors()) {
+			return "/login/loginForm";
+		}
+		
+		Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+		
+		if(loginMember == null) {
+			bindingResult.reject("loginFailed", "아이이 또는 비밀번호가 맞지 않습니다.");
+			return "/login/loginForm";
+		}
+		
+		//세션이 있으면 있는 세션 반환, 없으면 신규 세션 생성 - true(default), false는 세션이 있으면 반환하고, 없으면 신규 세션을 만들지 않는다. 
+		HttpSession session = request.getSession();
+		//세션에 로그인 회원 정보 보관
+		session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+		
+		return "redirect:/";
+	}
+	
+	//  @PostMapping("/logout")
+	public String logout(HttpServletResponse response) {
+		expireCookie(response, "memberId");
+		return "redirect:/";
+	}
+  
+//	@PostMapping("/logout")
+	public String logoutV2(HttpServletRequest request) {
+		sessionManager.expire(request);
+		return "redirect:/";
+	}
+	
+	@PostMapping("/logout")
+	public String logoutV3(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		if(session != null) {
+			session.invalidate();
+		}
+		return "redirect:/";
+	}
+  
+	private void expireCookie(HttpServletResponse response, String cookieName) {
+		Cookie cookie = new Cookie("memberId", null);
+		cookie.setMaxAge(0);
+		response.addCookie(cookie);
 	}
 }
